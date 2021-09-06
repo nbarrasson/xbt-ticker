d44be9e73c3455ec93d000c2f5cd5fa718ce1c3c
nibarras <nibarras@microsoft.com>
2021-09-06 16:05:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
+// function getRate() {
+//   var webSocket = $.simpleWebSocket({ url: 'wss://apim-nibarras-dev.azure-api.net/kraken' });
+//   webSocket.send({ "event": "subscribe", "pair": ["XBT/USD"], "subscription": { "name": "ticker" } }).done(function () {
+//     // message send
+//   }).fail(function (e) {
+//     // error sending
+//   });
+//   //var webSocket = $.simpleWebSocket({ url: 'wss://ws.kraken.com/' });
+//   webSocket.listen(function (message) {
+//     if (!Array.isArray(message)) return;
+//     var rate = message[1].c[0];
+//     if (rate) return (rate);
+//   })
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" color="primary">
+      {getRate()}
+    </Button>
   );
 }
 
