8e48374aa73d905bbe978eb048eabebd38a6cdc4
Nicolas Barrasson <nibarras@microsoft.com>
2021-09-06 16:06:58 +0000

    EOD commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-
-// function getRate() {
-//   var webSocket = $.simpleWebSocket({ url: 'wss://apim-nibarras-dev.azure-api.net/kraken' });
-//   webSocket.send({ "event": "subscribe", "pair": ["XBT/USD"], "subscription": { "name": "ticker" } }).done(function () {
-//     // message send
-//   }).fail(function (e) {
-//     // error sending
-//   });
-//   //var webSocket = $.simpleWebSocket({ url: 'wss://ws.kraken.com/' });
-//   webSocket.listen(function (message) {
-//     if (!Array.isArray(message)) return;
-//     var rate = message[1].c[0];
-//     if (rate) return (rate);
-//   })
-// }
+import React, { useState, useEffect, useRef } from 'react';
+const WebSocket = require('isomorphic-ws');
 
 function App() {
+  const [rate, setRate] = useState([0]);
+  const ws = useRef(null);
+  useEffect(() => {
+    ws.current = new WebSocket('wss://apim-nibarras-dev.azure-api.net/kraken');
+    ws.current.onopen = function open() {
+      console.log('connected');
+      ws.current.send('{ "event": "subscribe", "pair": ["XBT/USD"], "subscription": { "name": "ticker" } }');
+    };
+    ws.current.onclose = function close() {
+      console.log('disconnected');
+    };
+    ws.current.onmessage = function incoming(message) {
+      const parsedData = JSON.parse(message.data)
+      // console.log(parsedData);
+      if (!Array.isArray(parsedData)) return;
+      setRate([...rate, parsedData[1].c[0]]);
+    };
+  }, [])
+
   return (
     <Button variant="contained" color="primary">
-      Hello World!
-      // {getRate()}
+      {rate[rate.length - 1]}
     </Button>
   );
 }
