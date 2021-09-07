08111ec882f6d98cbf81fcb6c0abfd2dc8207114
Nicolas Barrasson <nibarras@microsoft.com>
2021-09-07 12:36:41 +0000

    final commit

--- src/client.js
+++ src/client.js
@@ -1,44 +1,37 @@
 import ReactDOM from 'react-dom';
 import React, { useState, useEffect, useRef } from 'react';
-import {
-  Line,
-  LineChart,
-  XAxis,
-  YAxis
-} from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const WebSocket = require('isomorphic-ws');
 
+var ws = new WebSocket('wss://apim-nibarras-dev.azure-api.net/kraken');
+ws.onopen = function open() {
+  console.log('connected to ' + ws.url);
+  ws.send('{ "event": "subscribe", "pair": ["XBT/USD"], "subscription": { "name": "ticker" } }');
+};
+ws.onclose = function close() {
+  console.log('disconnected');
+};
+
 const App = ({ }) => {
-  const [rate, setRate] = useState([]);
-  const ws = useRef(null);
+  const [tick, setTick] = useState([]);
   useEffect(() => {
-    ws.current = new WebSocket('wss://apim-nibarras-dev.azure-api.net/kraken');
-    ws.current.onopen = function open() {
-      console.log('connected');
-      ws.current.send('{ "event": "subscribe", "pair": ["XBT/USD"], "subscription": { "name": "ticker" } }');
-    };
-    ws.current.onclose = function close() {
-      console.log('disconnected');
-    };
-    ws.current.onmessage = function incoming(message) {
+    ws.onmessage = function incoming(message) {
       const parsedData = JSON.parse(message.data)
       if (!Array.isArray(parsedData)) return;
-      setRate(currentRate => [...currentRate, parsedData[1].c[0]]);
+      setTick(currentTick => [...currentTick, { xbt: Number(parsedData[1].c[0]), time: Date.now() }]);
     };
   }, [])
-  console.log(rate);
   return (
     <div>
-      <h1>Real Time XBT</h1>
-      {rate}
-      <LineChart width={500} height={300} data={rate}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Line dataKey="value" />
+      <h1>Real Time XBT/USD exchange rate</h1>
+      <LineChart width={1000} height={300} data={tick}>
+        <Line type="monotone" dataKey="xbt" stroke="#8884d8" dot={false} />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis hide="true" dataKey="time" interval="preserveEnd" />
+        <YAxis type="number" domain={['dataMin', 'dataMax']} />
       </LineChart>
     </div>
   );
 };
-
 ReactDOM.render(<App />, document.getElementById('root'));
